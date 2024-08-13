9e561c577309835f8e16f8893de7b55e34c9a009
adletb7 <adlet.baiteliyev@gmail.com>
2024-08-13 10:16:36 +0500

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         <div className='logo'>
           <img src={LogoIcon} alt='more' className="logo-icon"/>
         </div>
-        <nav>
+        <nav className='navStyle'>
         <ul className='sidebar'>
             <li><a href="#home">Home</a></li>
             <li><a href="#contact">Contact</a></li>
@@ -29,7 +29,7 @@ function App() {
             <li><a href="#about">About</a></li>
           </ul>
           <ul className='more-icon'>
-            <li><a href="#more" onClick={toggleSidebar}>
+            <li className='more-icon-li'><a href="#more" onClick={toggleSidebar}>
               <img src={MoreIcon} alt='more' className="menu-icon"/>
             </a></li>
           </ul>
